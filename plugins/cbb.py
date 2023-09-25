fac1c2ee7c937f31cbfef03bd3deb1af17f203e9
Dhruv <142153463+akagamikaizoku@users.noreply.github.com>
2023-09-25 17:52:09 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/akagamikaizoku'>Click here</a>\n○ Channel : @Otaku_Oracle\n○ Support Group : @otaku_oracle_supportgroup </b>",
+            text = f"<b>○ Creator : @eren_yeagerattacktitan\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Click here</a>\n○ Channel : @Otaku_Oracle\n○ Support Group : @otaku_oracle_supportgroup </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [

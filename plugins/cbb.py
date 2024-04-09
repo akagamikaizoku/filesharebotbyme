e721520ff0f6c4386ce3c3fa9d33c8f71c706172
Dhruv <142153463+akagamikaizoku@users.noreply.github.com>
2024-04-09 16:22:58 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : @eren_yeagerattacktitan\n○ Bot number = 1 \n○ Channel : @Otaku_Oracle\n○ Support Group : @otaku_oracle_supportgroup </b>",
+            text = f"<b>○ Creator : @eren_yeagerattacktitan\n○ Bot number: 1 \n○ Channel : @Otaku_Oracle\n○ Support Group : @otaku_oracle_supportgroup </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
